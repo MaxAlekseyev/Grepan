9a35b703c630dec9026baeac06ebaa33af660ebc
Max Alekseyev <maxalekseyev@gmail.com>
2017-06-17 14:33:42 +0300

    016-Related-products-arrows done

--- js/smartwave/jquery/plugins/owl-carousel/owl.carousel.js
+++ js/smartwave/jquery/plugins/owl-carousel/owl.carousel.js
@@ -405,7 +405,7 @@ if (typeof Object.create !== "function") {
         buildButtons : function () {
             var base = this,
                 buttonsWrapper = $("<div class=\"owl-buttons\"/>");
-            base.owlControls.append(buttonsWrapper);
+            base.owlControls.prepend(buttonsWrapper);
 
             base.buttonPrev = $("<div/>", {
                 "class" : "owl-prev",
@@ -1197,7 +1197,7 @@ if (typeof Object.create !== "function") {
                 iterations += 1;
                 if (base.completeImg($lazyImg.get(0)) || isBackgroundImg === true) {
                     showImage();
-                } else if (iterations <= 100) {//if image loads in less than 10 seconds 
+                } else if (iterations <= 100) {//if image loads in less than 10 seconds
                     window.setTimeout(checkLazyImage, 100);
                 } else {
                     showImage();
@@ -1226,7 +1226,7 @@ if (typeof Object.create !== "function") {
                 iterations += 1;
                 if (base.completeImg($currentimg.get(0))) {
                     addHeight();
-                } else if (iterations <= 100) { //if image loads in less than 10 seconds 
+                } else if (iterations <= 100) { //if image loads in less than 10 seconds
                     window.setTimeout(checkImage, 100);
                 } else {
                     base.wrapperOuter.css("height", ""); //Else remove height attribute

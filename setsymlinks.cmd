a36feb73ace85fb222715030874f4fb153f75d77
Max Alekseyev <maxalekseyev@gmail.com>
2017-06-24 08:09:35 +0300

    setsymlinks changed

--- setsymlinks.cmd
+++ setsymlinks.cmd
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 sharedDir=/home/magento/www/grepan.ua/shared
-workingDir=/home/magento/www/grepan.ua/releases/maxa
+workingDir=/home/magento/www/grepan.ua/releases/max.a
 
-ln -s ${sharedDir}/apc.php ${workingDir}/apc.php
-ln -s ${sharedDir}/app/etc/local.xml ${workingDir}/app/etc/local.xml
+ln -sf ${sharedDir}/apc.php ${workingDir}/apc.php
+ln -sf ${sharedDir}/app/etc/local.xml ${workingDir}/app/etc/local.xml
 
 
-ln -s ${sharedDir}/media ${workingDir}/media
-ln -s ${sharedDir}/sitemaps ${workingDir}/sitemaps
-ln -s ${sharedDir}/staging ${workingDir}/staging
-ln -s ${sharedDir}/var ${workingDir}/var
+ln -sf ${sharedDir}/media ${workingDir}/media
+ln -sf ${sharedDir}/sitemaps ${workingDir}/sitemaps
+ln -sf ${sharedDir}/staging ${workingDir}/staging
+ln -sf ${sharedDir}/var ${workingDir}/var
